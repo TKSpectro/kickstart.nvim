ea5882e89b7f695774cf8b28cdff0b1430f6210c
Tom Kaeppler <tomkaeppler@web.de>
2023-10-10 17:18:18 +0200

    enable auto import and add some basic stuff from old config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Enable relative line numbers
+vim.o.relativenumber = true
+vim.o.scrolloff = 8
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+
 return {}
